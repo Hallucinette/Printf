3fe634081bd94d324ba6fc2da96d91199e26903b
Amélie POCCHIOLO <ameliepocchiolo@MacBook-Pro.home>
2020-11-17 20:10:25 +0100

    type d, c, s √ 17/11/20

--- ft_convertion.c
+++ ft_convertion.c
@@ -23,6 +23,67 @@ void		ft_write_c(t_data *p)
 		ft_write_width(p);
 }
 
+void		ft_write_s(t_data *p)
+{
+ 	char  *string;
+	int i;
+	int len;
+
+	string = va_arg(p->list, char *);
+	i = 0;
+	
+	if (string != NULL)
+		len = ft_strlen(string);
+	if (string == NULL)
+	{
+		string = "(null)";
+		len = ft_strlen(string);
+	}
+	if (len >= p->width && (len <= p->precision || p->precision == -1))
+	{
+		ft_putstr_fd(string, 1);
+		p->size += len;
+	}
+	else
+	{
+		if (p->precision >= 0 && p->precision < len && p->precision >= p->width)
+		{
+			while (p->precision > i)
+			{
+				ft_putchar_fd(string[i], 1);
+				i++;
+			}
+			p->size += p->precision;
+		}
+		else
+		{
+			if (p->precision < len && p->precision >= 0)
+				p->width -= p->precision;
+			else
+				p->width = (p->width - len);
+			if (p->minZ != 2)
+				ft_write_width(p);
+			if (p->precision < len && p->precision >= 0)
+			{
+				while (p->precision > i)
+				{
+					ft_putchar_fd(string[i], 1);
+					i++;
+				}
+				p->size += p->precision;
+			}
+			else
+			{
+				ft_putstr_fd(string, 1);
+				p->size += len;
+			}
+			if (p->minZ == 2)
+				ft_write_width(p);
+		}
+		
+	}
+}
+
 void	ft_print_pre(t_data *p, int num)
 {
 	if (num < 0)
@@ -95,99 +156,38 @@ void	ft_write_d(t_data *p)
 			}
 		}
 	}
+}
 
+/* void		ft_write_Xx(t_data *p)
+{
+	unsigned int  adr;
+	char          *base;
+	char          res[9];
+	int           i;
 	
-	/*if (p->minZ != 2)
+	adr = va_arg(p->list, unsigned int);
+	base = "0123456789abcdef";
+	i = 8;
+	while ((adr / 16) > 0) //|| i >= 8)
 	{
-		if (num < 0 && p->minZ == 1)
-			num = ft_num_neg(p, num);
-
-	 	//ft_write_width(p, ft_intlen(num));
+    	res[i] = base[(adr % 16)];
+    	adr /= 16;
+    	i--;	
 	}
-	if (p->precision >= 1)
-	{
-	}
-	// p->size += ft_intlen(num);
-	// ft_putnbr_fd(num, 1);
-
-	if (p->minZ == 2)
-		//ft_write_width(p, ft_intlen(num));*/
-}
-/*
-
-void	ft_write_d(t_data *p)
-{
-	int  num;
-	num = va_arg(p->list, int);
-		//printf("\nnum neg:%i\n", num);
+	res[i] = base[(adr % 16)];
 	if (p->minZ != 2)
+		//ft_write_width(p, 9 - i);
+	p->size += 9 - i;
+	while (i < 9)
 	{
-		if (num < 0 && p->minZ == 1)
-			num = ft_num_neg(p, num);
-
-	 	//ft_write_width(p, ft_intlen(num));
-	}
-	p->size += ft_intlen(num);
-	ft_putnbr_fd(num, 1);
-	// p->size += ft_intlen(num);
-	// ft_putnbr_fd(num, 1);
-
-	if (p->minZ == 2)
-		//ft_write_width(p, ft_intlen(num));
-}*/
-// void	ft_write_d(t_data *p)
-// {
-// 	int  num;
-// 	num = va_arg(p->list, int);
-// 		//printf("\nnum neg:%i\n", num);
-// 	if (p->minZ != 2)
-// 	{
-// 		if (num < 0 && p->minZ == 1)
-// 			num = ft_num_neg(p, num);
-
-// 	 	//ft_write_width(p, ft_intlen(num));
-// 	}
-// 	// p->size += ft_intlen(num);
-// 	// ft_putnbr_fd(num, 1);
-
-// 	if (p->minZ == 2)
-// 		//ft_write_width(p, ft_intlen(num));
-// }
-
-// void		ft_write_s(t_data *p)
-// {
-// 	char  *string;
-// 	//int i = 0;
-// 	string = va_arg(p->list, char *);
-
-// 	//ft_write_width(p, ft_strlen(string));
-// 	p->size += ft_strlen(string);
-// 	if (p->precision > 0)
-// 	{
-// 		while (p->precision > 0)
-// 		{
-// 			ft_putstr_fd(string, 1);
-// 			p->precision--;
-
-// 		}
-		
-// 	}
-// 	// else 
-// 	// 		ft_putstr_fd(string, 1);
-// }
-
-void		ft_write_s(t_data *p)
-{
-	char  *string;
-	string = va_arg(p->list, char *);
-    //printf("1");
-	if (p->minZ != 2)
-	 	//ft_write_width(p, ft_strlen(string));
-	p->size += ft_strlen(string);
-	ft_putstr_fd(string, 1);
+		if(*p->format == 'X')
+			res[i] = ft_toupper(res[i]);
+	 	write(1, &res[i], 1);
+		i++;
+	}	
 	//if (p->minZ == 2)
-		//ft_write_width(p, ft_strlen(string));
-}
+		//ft_write_width(p, 9 - i);
+} */
 
 void		ft_write_Xx(t_data *p)
 {
@@ -204,11 +204,8 @@ void		ft_write_Xx(t_data *p)
     	res[i] = base[(adr % 16)];
     	adr /= 16;
     	i--;	
-		//printf("res: %s\ni: %d\n", res, i);
 	}
 	res[i] = base[(adr % 16)];
-	//printf("res: %s\nlen: %d\n", res, (int)ft_strlen(res));
-	//write(1, "0x10", 4);					// modificar
 	if (p->minZ != 2)
 		//ft_write_width(p, 9 - i);
 	p->size += 9 - i;
@@ -221,4 +218,4 @@ void		ft_write_Xx(t_data *p)
 	}	
 	//if (p->minZ == 2)
 		//ft_write_width(p, 9 - i);
-} 
+}
--- main.c
+++ main.c
@@ -33,37 +33,64 @@ int main()
     // ft_printf("%07d\n", -54);
 
 
-    // printf("\n");
-    // printf("//Type Xx//\n");
-
-    // printf("Nor : %x\n", b);
-    // ft_printf("Nor : %x\n", b);
-    // printf("R.. : %8x\n", 163);
-    // ft_printf("F.. : %8x\n", 163);
-    // printf("R.0 : %03X\n", 1034);
-    // ft_printf("F.0 : %03X\n", 1034);
-
     printf("\n");
-    printf("//Type s//\n");
-    printf("Nor : %s\n", s);
-    ft_printf("Nor : %s\n", s);
-    printf("R.. : %5s\n", s);
-    ft_printf("F.. : %5s\n", s);
-    printf("\n//precision//\n");
-    printf("%1.2s\n", s);
-    ft_printf("%1.2s\n", s);
-    //printf("%0-0s", s);
-    //printf("%-8.5x", 34);
+    printf("//Type Xx//\n");
+    printf("Nor  : |%x|\n", 123456789);
+    ft_printf("Nor  : |%x|\n", 123456789);
+    printf("\n//Width//\n");
 
-   
+
+    printf("R.3  : |%3X|\n", 123456789);
+    //ft_printf("F.3  : |%3X|\n", 123456789);
+    printf("R.7  : |%7x|\n", 123456789);
+    //ft_printf("F.7  : |%7x|\n", 123456789);
+    printf("R.9  : |%9x|\n", 123456789);
+    //ft_printf("F.9  : |%9x|\n", 123456789);
+    printf("R.11 : |%11x|\n", 123456789);
+    //ft_printf("F.11 : |%11x|\n", 123456789);
+
+    printf("\n//precision//\n");
+
+    printf("R.0  : |%.0x|\n", 123456789);
+    //ft_printf("F.0  : |%.0x|\n", 123456789);
+    printf("R.1  : |%.1x|\n", 123456789);
+    //ft_printf("F.1   : |%.1x|\n", 123456789);
+    printf("R.7  : |%.7x|\n", 123456789);
+    //ft_printf("F.7   : |%.9x|\n", 123456789);
+    printf("R.9  : |%.10x|\n", 123456789);
+    //ft_printf("F.9   : |%.15x|\n", 123456789);
+
+   /* printf("\n");
+    printf("//Type s//\n");
+    printf("Nor : |%s|\n", s);
+    ft_printf("Nor : |%s|\n", s);
+
+    printf("//Width//\n");
+    printf("R.. : |%4s|\n", s);
+    ft_printf("F.. : |%4s|\n", s);
+    printf("R.. : |%9s|\n", s);
+    ft_printf("F.. : |%9s|\n", s);
+    printf("R.. : |%-9s|\n", s);
+    ft_printf("F.. : |%-9s|\n", s);
+
+    printf("\n//precision//\n");
+    printf("R.. : |%6s|\n", NULL);
+    ft_printf("F.. : |%4.4s|\n", NULL);
+    printf("R.. : |%9.2s|\n", s);
+    ft_printf("F.. : |%9.2s|\n", s);
+    printf("R.. : |%-9.2s|\n", s);
+    ft_printf("F.. : |%-9.2s|\n", s);
+    printf("R.. : |%3.5s|\n", s);
+    ft_printf("F.. : |%3.5s|\n", s);
+   */
 
 //    printf("ast|%2.2d|\n", -1);
 //    printf("ast|%3.2d|\n", -1);
-   printf("ast|%-5d|\n", 143);
-   ft_printf("asf|%-5d|\n", 143);
-   printf("ast|%-5.1d|\n", 14);
-   ft_printf("asf|%-5.1d|\n", 14);
-   ft_printf("asf|%2.3d|\n", -1);
+//    printf("ast|%-5d|\n", 143);
+//    ft_printf("asf|%-5d|\n", 143);
+//    printf("ast|%-5.1d|\n", 14);
+//    ft_printf("asf|%-5.1d|\n", 14);
+//    ft_printf("asf|%2.3d|\n", -1);
   // printf("ast|%4.4d|\n", -1);
 //    printf("ast|%04d|\n", 555);
 //    printf("ast|%4.2d|\n", -55);
@@ -72,9 +99,23 @@ int main()
 //    ft_printf("ast|%-*d|\n", 3, 5); 
 
 
-   printf("ast|%*.*d|\n", 5, 3, 13);
-    // printf("R.0 : %05s\n", s);
-    // ft_printf("F.0 : %05x\n", b);
+  // printf("ast|%*.*d|\n", 5, 3, 13);
+  // printf("R.0 : %05s\n", s);
+  // ft_printf("F.0 : %05x\n", b);
+
+  // printf("//// NORMAL ////");
+  // printf("\n");
+  // printf("R. : %p\n", a);
+  // ft_printf("F. : %p\n", a);
+
+    //printf("//// WITH ////");
+    // printf("\n");
+    // printf("//Type C//\n");
+
+    // printf("R.. : %4c\n", a);
+    // ft_printf("F.. : %4c\n", a);
+    // printf("R.0 : %4c\n", a);
+    // ft_printf("F.0 : %4c\n", a);
 
 
 
@@ -85,10 +126,10 @@ int main()
 
 
 
-    printf("-%-5.i\n", 0);
-    printf("-%-5.0i\n", 0);
-    printf("-%-5.0i\n", 2);
-    printf("-%-5.2i\n", 0);
+    // printf("-%-5.i\n", 0);
+    // printf("-%-5.0i\n", 0);
+    // printf("-%-5.0i\n", 2);
+    // printf("-%-5.2i\n", 0);
     //ft_printf("falso : %i\n", b);
     //ft_printf("falso : %d\n", c);
     //ft_printf("falso : %i\n", c);
