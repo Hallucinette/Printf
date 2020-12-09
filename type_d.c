6be41e4ee29076dfe5d27c195fd36de96acee323
Amélie POCCHIOLO <ameliepocchiolo@MacBook-Pro.home>
2020-12-10 00:15:02 +0100

    remise a 0

---
+++ type_d.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   type_d.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amepocch <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/09 22:42:38 by amepocch          #+#    #+#             */
+/*   Updated: 2020/12/09 22:48:44 by amepocch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int		ft_num_neg(t_data *p, int num)
+{
+	ft_putchar_fd('-', 1);
+	p->size++;
+	num *= -1;
+	return (num);
+}
+
+void	ft_write_d(t_data *p)
+{
+	int		num;
+
+	num = va_arg(p->list, int);
+	if (ft_intlen(num) >= p->width && ft_intlen(num) >= p->precision)
+	{
+		if (!(p->precision == 0 && num == 0))
+		{
+			p->size += ft_intlen(num);
+			ft_putnbr_fd(num, 1);
+		}
+	}
+	else
+	{
+		if (p->precision >= p->width)
+			ft_print_pre(p, num);
+		else
+		{
+			if (ft_intlen(num) > p->precision &&
+					!(p->precision == 0 && num == 0))
+				p->width = (p->width - ft_intlen(num));
+			else
+			{
+				p->width = (p->width - p->precision);
+				if (num < 0)
+					p->width--;
+			}
+			ft_write_d2(p, num);
+		}
+	}
+}
+
+void	ft_write_d2(t_data *p, int num)
+{
+	if (p->minZ != 2)
+	{
+		if (num < 0 && p->minZ == 1 && p->precision == -1)
+			num = ft_num_neg(p, num);
+		ft_write_width(p);
+	}
+	if (p->precision > 0)
+		ft_print_pre(p, num);
+	else
+	{
+		if (!(p->precision == 0 && num == 0))
+		{
+			ft_putnbr_fd(num, 1);
+			p->size += ft_intlen(num);
+		}
+	}
+	if (p->minZ == 2)
+		ft_write_width(p);
+}
---
+++ type_x.c
@@ -0,0 +1,94 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   type_xX.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amepocch <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/09 23:02:56 by amepocch          #+#    #+#             */
+/*   Updated: 2020/12/09 23:06:42 by amepocch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+void	ft_put_x(t_data *p, int len)
+{
+	int	i;
+
+	i = 0;
+	if (*p->format == 'X')
+	{
+		while (p->resp[i])
+		{
+			p->resp[i] = ft_toupper(p->resp[i]);
+			i++;
+		}
+	}
+	write(1, &(p->resp), len);
+}
+
+void	ft_write_x(t_data *p)
+{
+	int				len;
+	unsigned int	nbr;
+
+	nbr = va_arg(p->list, unsigned int);
+	len = putnbr_base(nbr, "0123456789abcdef", p);
+	if (len >= p->width && len >= p->precision)
+	{
+		if (!(p->precision == 0 && nbr == 0))
+		{
+			p->size += len;
+			ft_put_xX(p, len);
+		}
+	}
+	else
+		ft_write_x2(p, len, nbr);
+}
+
+void	ft_write_x2(t_data *p, int len, unsigned int nbr)
+{
+	if (p->precision >= len && p->precision >= p->width && p->precision > 0)
+	{
+		p->size += p->precision;
+		p->precision -= len;
+		while (p->precision > 0)
+		{
+			write(1, "0", 1);
+			p->precision--;
+		}
+		ft_put_x(p, len);
+	}
+	else
+	{
+		if (p->precision <= len)
+		{
+			p->size += len;
+			p->width -= len;
+		}
+		else
+		{
+			p->size += p->precision;
+			p->width -= p->precision;
+		}
+		ft_write_x3(p, len, nbr);
+	}
+}
+
+void	ft_write_x3(t_data *p, int len, unsigned int nbr)
+{
+	if (p->minZ != 2)
+		ft_write_width(p);
+	while (p->precision > len)
+	{
+		ft_putchar_fd('0', 1);
+		p->precision--;
+	}
+	if (p->precision == 0 && nbr == 0)
+		ft_putchar_fd(' ', 1);
+	else
+		ft_put_x(p, len);
+	if (p->minZ == 2)
+		ft_write_width(p);
+}
